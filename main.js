f7e8853a56ad93253d7f96307292f126882f3bf5
Blank-ally <lia.stith@gmail.com>
2024-11-08 13:41:42 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function (){
+
+    const params = {
+        q: 'javascript',
+        maxResults:24,
+    }
+$.get(
+    '',
+    params,
+    function(data){
+
+    },
+    'json'
+)
+
+})
