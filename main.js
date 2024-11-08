f74e24bbd09aef6552fb8c8f20f8c1731d4a6a7b
Blank-ally <lia.stith@gmail.com>
2024-11-08 15:31:54 -0600

    Working just need to  refactor the look and work on setting a default so  the page has results on load maybe a page header

--- main.js
+++ main.js
@@ -4,13 +4,43 @@ $(document).ready(function (){
         q: 'javascript',
         maxResults:24,
     }
-$.get(
-    '',
-    params,
-    function(data){
+$('#Search').on('submit', function(e){
 
-    },
-    'json'
-)
+    e.preventDefault()
+
+
 
 })
+
+    let $search = $('#newSearch').val().replaceAll(' ','+')
+    console.log($search)
+    $.get(
+        'https://itunes.apple.com/search?term=' + $search,
+        params,
+        function(data){
+            console.log('results',data)
+            console.log(data.results[0].collectionName)
+
+            for( let item of data.results){
+
+                $('#results').append(`
+                  <div class="col">
+            <div class="card">
+                <img src="${item.artworkUrl100}" class="card-img-top" >
+                <div class="card-body">
+                    <h5 class="card-title">${item.collectionName}</h5>
+                    <h6>${item.artistName}</h6>
+                    <p>${item.wrapperType}</p>
+                </div>
+            </div>
+        </div>
+                `)
+
+            }
+
+        },
+        'json'
+    );
+
+    console.log('done loading')
+});
